f0a7e668b28038a6526926606f35d6a5c87f705d
Your Namealt1tla <GerdzhikovaVeronWench@gmail.com>
2024-08-10 23:53:00 +0400

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# weather-widget
-To get the weather just enter a location.
+# Weather widget version: 1.0.0
+
+This version includes some functionality:
+
+- Weather info
+- Visual effects
+
+## Weather info
+
+Weather information depending on the entered location.
+
+### Parameters
+
+- pressure
+- humidity
+- wind speed
+- temp min
+- temp max
+
+## Visual effects
+
+In addition to the displayed text information, an image corresponding to the weather conditions is also displayed.
--- main.js
+++ main.js
@@ -1,36 +1,37 @@
-let apiKey = '017106187624b141cf038a4c591760ec';
-let apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&lang=en';
+let apiKey = "017106187624b141cf038a4c591760ec";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=en";
 
 let form = document.getElementById("form");
-let searchLocation = document.getElementById("search")
+let searchLocation = document.getElementById("search");
 let searchButton = document.querySelector(".loc-button");
 let weatherIcon = document.getElementById("weather-icon");
-let todayDay = document.querySelector('.today-day');
-let todayDate = document.querySelector('.today-date');
+let todayDay = document.querySelector(".today-day");
+let todayDate = document.querySelector(".today-date");
 
 let days = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
 ];
 
 let mounths = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'June',
-    'July',
-    'Aug',
-    'Sept',
-    'Oct',
-    'Now',
-    'Dec'
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sept",
+  "Oct",
+  "Now",
+  "Dec",
 ];
 
 let d = new Date();
@@ -39,51 +40,59 @@ let m = d.getMonth();
 todayDay.innerText = days[n];
 todayDate.innerText = `${d.getDate()} ${mounths[m]} ${d.getFullYear()}`;
 
-
 async function checkWeather(city) {
-    try {
-        const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
+  try {
+    const response = await fetch(`${apiUrl}&q=${city}&appid=${apiKey}`);
 
-        if (!response.ok) {
-            throw new Error("Unable to fetch weather data.");
-        }
-
-        const data = await response.json();
-
-        console.log(data)
-
-        document.querySelector(".weather-temp").innerText = Math.round(data.main.temp) + " °C";
-        document.querySelector(".weather-main").innerText = data.weather[0].main;
-        document.querySelector(".city").innerText = data.name;
-        document.querySelector(".pressure").innerText = data.main.pressure + " hPa";
-        document.querySelector(".humidity").innerText = data.main.humidity + " %";
-        document.querySelector(".wind-speed").innerText = data.wind.speed + " m/s";
-        document.querySelector(".temp-min").innerText = Math.round(data.main.temp_min) + " °C";
-        document.querySelector(".temp-max").innerText = Math.round(data.main.temp_max) + " °C";
-
-        weatherIcon.classList.remove('fa-location-crosshairs', 'fa-cloud', 'fa-sun', 'fa-cloud-showers-heavy', 'fa-cloud-rain', 'fa-smog');
-        if (data.weather[0].main === "Clouds") {
-            weatherIcon.classList.add("fa-cloud");
-        } else if (data.weather[0].main === "Clear") {
-            weatherIcon.classList.add("fa-sun");
-        } else if (data.weather[0].main === "Rain") {
-            weatherIcon.classList.add("fa-cloud-showers-heavy");
-        } else if (data.weather[0].main === "Drizzle") {
-            weatherIcon.classList.add("fa-cloud-rain");
-        } else if (data.weather[0].main === "Mist") {
-            weatherIcon.classList.add("fa-smog");
-        }
-
-    } catch (error) {
-        console.error(error);
+    if (!response.ok) {
+      throw new Error("Unable to fetch weather data.");
     }
+
+    const data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".weather-temp").innerText =
+      Math.round(data.main.temp) + " °C";
+    document.querySelector(".weather-main").innerText = data.weather[0].main;
+    document.querySelector(".city").innerText = data.name;
+    document.querySelector(".pressure").innerText = data.main.pressure + " hPa";
+    document.querySelector(".humidity").innerText = data.main.humidity + " %";
+    document.querySelector(".wind-speed").innerText = data.wind.speed + " m/s";
+    document.querySelector(".temp-min").innerText =
+      Math.round(data.main.temp_min) + " °C";
+    document.querySelector(".temp-max").innerText =
+      Math.round(data.main.temp_max) + " °C";
+
+    weatherIcon.classList.remove(
+      "fa-location-crosshairs",
+      "fa-cloud",
+      "fa-sun",
+      "fa-cloud-showers-heavy",
+      "fa-cloud-rain",
+      "fa-smog"
+    );
+    if (data.weather[0].main === "Clouds") {
+      weatherIcon.classList.add("fa-cloud");
+    } else if (data.weather[0].main === "Clear") {
+      weatherIcon.classList.add("fa-sun");
+    } else if (data.weather[0].main === "Rain") {
+      weatherIcon.classList.add("fa-cloud-showers-heavy");
+    } else if (data.weather[0].main === "Drizzle") {
+      weatherIcon.classList.add("fa-cloud-rain");
+    } else if (data.weather[0].main === "Mist") {
+      weatherIcon.classList.add("fa-smog");
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const city = searchLocation.value.trim();
-    if (city !== "") {
-        checkWeather(city);
-        searchLocation.value = '';
-    }
-})
+  e.preventDefault();
+  const city = searchLocation.value.trim();
+  if (city !== "") {
+    checkWeather(city);
+    searchLocation.value = "";
+  }
+});
